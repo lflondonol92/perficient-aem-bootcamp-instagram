bba3bb419dd77c3c06f3a71bbfddcd0d94de1a1c
cmsamayoa <Manuel.Samayoa@smith.co>
2021-04-20 13:48:51 -0600

    TBCM-1 added fixes to model for edges.

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/dto/instagram/ShortCodeMedia.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/dto/instagram/ShortCodeMedia.java
@@ -1,9 +1,13 @@
 package com.adobe.aem.guides.wknd.core.models.dto.instagram;
 
+import com.google.gson.Gson;
+
 public class ShortCodeMedia extends Node {
 
     private EdgeChildren edge_media_to_caption;
     private EdgeChildren edge_sidecar_to_children;
+    private Owner owner;
+    private final Gson gson = new Gson();
 
     public ShortCodeMedia(){
     }
@@ -22,5 +26,34 @@ public class ShortCodeMedia extends Node {
 
     public void setEdge_sidecar_to_children(EdgeChildren edge_sidecar_to_children) {
         this.edge_sidecar_to_children = edge_sidecar_to_children;
+    }
+
+    public String getEdgeMediaToCaption(){
+        if(edge_media_to_caption != null){
+            return gson.toJson(edge_media_to_caption);
+        }
+        return null;
+    }
+
+    public String getEdgeSidecarToChildren(){
+        if(edge_sidecar_to_children != null){
+            return gson.toJson(edge_sidecar_to_children);
+        }
+        return null;
+    }
+
+    public String getEdgeOwner(){
+        if(owner !=null){
+            return gson.toJson(owner);
+        }
+        return null;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
     }
 }
