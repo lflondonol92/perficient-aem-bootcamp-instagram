4a6b30dc044a428b6f3e60f23d435dca917d28ad
cmsamayoa <Manuel.Samayoa@smith.co>
2021-04-21 13:41:01 -0600

    TBCM-1 added modal close button. Added modal styles for header and image container.

--- ui.frontend/src/main/webpack/components/instagram-feed-list/instagrampost.js
+++ ui.frontend/src/main/webpack/components/instagram-feed-list/instagrampost.js
@@ -2,30 +2,111 @@ var jQuery = require("jquery");
 
 jQuery(function ($) {
     "use strict";
-    /*function pullIgPost(){
-        const SELECTORS = {
-            IG_COMPONENT : ".cmp-instagram-post",
-        };
-        const token = "EAAC8ZCVnhe18BAPaip5EAcQn5XZAkOZB4IbwZCjBSQlXxm0fmsPi1yZCCVDAnRQqVou66BlO8Hhanhb2ZAzKWyzK3TvnaR4CyThqQScKHA70ZAsgPoOd5lNeGSHOnpTSDNZBqTXG7CgFk92f0TjZAOxzfs63gZAnErZAgVETWL2mvZBoCZApgV049uKlW1WahaoZC9Y0EYdxl2yHCf20wcXeZBoBQZCKmaPz0SXduycobFt5W873keP5PaVpzASSfDXOotuMm3AZD";
-        $(SELECTORS.IG_COMPONENT).each((_, item) => {
-            if($(item).data()){
-                const url = $(item).data("url");
-                const endpoint = "https://graph.facebook.com/v10.0/instagram_oembed";
 
-                $.get(endpoint, {access_token: token, debug: "all", format: "json", pretty:0, suppress_http_code: 1,
-                                  transport: "cors", url: url} )
-                    .done(function(data) {
-                        //console.log(data.author_name);
-                        if(data){
-                            $(item).html(data.html);
-                        }
-                });
-            }
-        });
-    }*/
+    let visible = false;
 
-    $( document ).ready(function() {
-        console.log('ready...');
+    /**
+     * Handle clicking of the Sign In button
+     */
+    $('body').on('click', '[data-ig-modal-url]', showModal);
+
+
+    /**
+     * Handle clicking "off-modal" to hide it
+     */
+    $(document).on('click', (hideModal));
+
+    /**
+     * Handle esc "off-modal" to hide it
+     */
+    $(document).on('keyup',function(evt) {
+        if (evt.keyCode == 27) {
+            hideModal(evt);
+        }
     });
+
+    function showModal(e) {
+        e.preventDefault();
+
+        //const xfUrl = $(this).data('ig-modal-url');
+        const xfUrl = 'http://localhost:4502/content/experience-fragments/wknd/language-masters/en/site/sign-in/master.content.html';
+        const typeName = $(this).data('type-name');
+        const displayUrl = $(this).data('display-url');
+        const sidecarToChildren = $(this).data('sidecar-to-children');
+        const mediaSection = typeName == 'GraphImage' ? createGraphImage(displayUrl) : createSidecar(displayUrl, sidecarToChildren);
+        const mediaToCaption = $(this).data('media-to-caption');
+        const owner = $(this).data('owner');
+
+        if (visible || !xfUrl) { return; }
+        const showModalEvt = new Event('wknd-ig-modal-show');
+        const body = document.querySelector('body');
+
+        const modal = $('<div id="wknd-ig-modal"/>')
+            .append(createHeaderModal())
+            .append(mediaSection);
+
+        $(modal).ready(function (){
+            $('#wknd-modal-close-btn').on('click', hideModal)
+        })
+
+        $('body').append(modal);
+
+        modal.fadeIn(300, function() { visible = true; });
+        visible = true;
+        // dispatch event to indicate that the modal has been shown
+        // used by sign-in-form.js to dynamically update a successful sign-in redirect to the current page
+        body.dispatchEvent(showModalEvt);
+
+        /*$.get(xfUrl, function (data) {
+            const modal = $('<div id="wknd-ig-modal"/>');
+
+            $('body').apui.frontend/src/main/webpack/components/instagram-feed-list/instagrampost.jspend(modal.append(data));
+            modal.fadeIn(300, function() { visible = true; });
+            visible = true;
+            // dispatch event to indicate that the modal has been shown
+            // used by sign-in-form.js to dynamically update a successful sign-in redirect to the current page
+            body.dispatchEvent(showModalEvt);
+        });*/
+
+        return false;
+    }
+
+    function createGraphImage(displayUrl) {
+        const container = $('<div class="cpm-instagram-modal__media"/>');
+        const image = $('<img class="cpm-instagram-modal__media-image">')
+            .attr("src", displayUrl);
+        return container.append(image);
+    }
+
+    function createSidecar(displayUrl, sidecarMedia) {
+        const container = $('<div class="cpm-instagram-modal__media"/>');
+        const image = $('<img class="cpm-instagram-modal__media-image">')
+            .attr("src", displayUrl);
+        return container.append(image);
+    }
+
+    function createHeaderModal(){
+        return $('<div class="cpm-instagram-modal__header" />')
+            .append($('<div class="cpm-instagram-modal__close" />')
+                .append($('<span id="wknd-modal-close-btn" class="close">x</span>')));
+    }
+
+    function hideModal(e) {
+        const modal = $('#wknd-ig-modal');
+        // if the target of the click isn't the modal nor a descendant of the modal
+        if (visible && modal && !modal.is(e.target) && (modal.has(e.target).length === 0 ||
+            (modal.has(e.target).length === 1 && $(e.target).attr('id') === 'wknd-modal-close-btn'))) {
+
+            e.preventDefault();
+
+            modal.fadeOut(200, function(){
+                modal.remove();
+                visible = false;
+            });
+
+            return false;
+        }
+
+    }
 
 });
