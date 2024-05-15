c860fc7220ed2428fa3aa0d9d53865b474fc3824
flondon <felipe.londono@daimler.com>
2024-05-15 16:08:18 -0500

    Annotation model added to the InstagramUserModelImpl

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/InstagramUserModelImpl.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/InstagramUserModelImpl.java
@@ -7,12 +7,21 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import lombok.Getter;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
 
+@Model(
+        adaptables = {SlingHttpServletRequest.class},
+        adapters = {InstagramUserModel.class},
+        resourceType = {InstagramFeedListImpl.RESOURCE_TYPE},
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
+)
 public class InstagramUserModelImpl implements InstagramUserModel {
 
     // Add a logger for any errors
